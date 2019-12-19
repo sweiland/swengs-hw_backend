05162b3f4a56a3160e42bde4f99373dc8331e870
Sebastian Weiland <hello@sweiland.at>
2019-12-19 18:07:03 +0100

    begin work on rest
    but enough for today

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend'
+    'backend',
+    'rest_framework',
+    'drf_yasg',
+    'config'
 ]
 
 MIDDLEWARE = [
