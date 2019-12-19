917033f4b8e04c229a4160d0f95df3815f5f074e
Sebastian Weiland <hello@sweiland.at>
2019-12-19 18:14:34 +0100

    finish serializers

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,4 +1,21 @@
 from rest_framework import serializers
 
+from backend.models import Member, Habit, Type
 
-class MemberListSerializer(serializers.ModelSerializer):
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = '__all__'
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = '__all__'
+
+
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = '__all__'
